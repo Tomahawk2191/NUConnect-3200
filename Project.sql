8ce667a123627f26d013fb642e05887951979516
pidang1 <dang.pi@northeastern.edu>
2024-11-19 18:57:49 -0500

    fixed errors

--- Project.sql
+++ Project.sql
@@ -11,7 +11,7 @@ CREATE TABLE school (
     #profilepic
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     roleId INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     canPost BOOLEAN DEFAULT false,
@@ -26,7 +26,7 @@ CREATE TABLE roles (
     canUpdateAccess BOOLEAN DEFAULT false
 );
 
-CREATE TABLE users (
+CREATE TABLE user (
     userId INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(50) NOT NULL,
     middleName VARCHAR(50),
@@ -39,10 +39,39 @@ CREATE TABLE users (
     lastLogin DATETIME DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (schoolId) REFERENCES school(schoolId),
-    FOREIGN KEY (roleId) REFERENCES roles(roleId)
+    FOREIGN KEY (roleId) REFERENCES role(roleId)
 );
 
-CREATE TABLE programs (
+CREATE TABLE userTagParent (
+    userTagId INT AUTO_INCREMENT PRIMARY KEY,
+    tagName VARCHAR(100),
+    category VARCHAR(100)
+);
+
+CREATE TABLE userTag (
+    userId INT NOT NULL,
+    userTagId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(userId)
+	    ON UPDATE cascade ON DELETE cascade,
+    FOREIGN KEY (userTagId) REFERENCES userTagParent(userTagId) 
+	    ON UPDATE cascade ON DELETE cascade
+);
+
+CREATE TABLE profile (
+    profileId INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    middleName VARCHAR(50),
+    lastName VARCHAR(50) NOT NULL,
+    userId INT,
+    schoolId INT,
+    bio VARCHAR(300),
+    #profilepic
+    email VARCHAR(254) NOT NULL UNIQUE,
+    FOREIGN KEY (userId) REFERENCES user(userId),
+    FOREIGN KEY (schoolId) REFERENCES school(schoolId)
+);
+
+CREATE TABLE program (
     programId INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (100) NOT NULL UNIQUE,
     description VARCHAR (300),
@@ -54,39 +83,10 @@ CREATE TABLE programs (
     programStart DATE,
     programEnd DATE,
     FOREIGN KEY (schoolId) REFERENCES school(schoolId),
-    FOREIGN KEY (professorId) REFERENCES users(userId)
+    FOREIGN KEY (professorId) REFERENCES user(userId)
 );
 
-CREATE TABLE userTagParent (
-    userTagId INT AUTO_INCREMENT PRIMARY KEY,
-    tagName VARCHAR(100),
-    category VARCHAR(100)
-);
-
-CREATE TABLE userTags (
-    userId INT NOT NULL,
-    userTagId INT NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(userId) 
-	    ON UPDATE cascade ON DELETE cascade,
-    FOREIGN KEY (userTagId) REFERENCES userTagParent(userTagId) 
-	    ON UPDATE cascade ON DELETE cascade
-);
-
-CREATE TABLE profiles (
-    profileId INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(50) NOT NULL,
-    middleName VARCHAR(50),
-    lastName VARCHAR(50) NOT NULL,
-    userId INT,
-    schoolId INT,
-    bio VARCHAR(300),
-    #profilepic
-    email VARCHAR(254) NOT NULL UNIQUE,
-    FOREIGN KEY (userId) REFERENCES users(userId),
-    FOREIGN KEY (schoolId) REFERENCES school(schoolId)
-);
-
-CREATE TABLE posts (
+CREATE TABLE post (
     postId INT AUTO_INCREMENT PRIMARY KEY,
     postAuthor VARCHAR(150) NOT NULL,
     title VARCHAR (100) NOT NULL,
@@ -101,8 +101,8 @@ CREATE TABLE posts (
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     lastEdited DATETIME DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (profileId) REFERENCES profiles(profileId),
-    FOREIGN KEY (programId) REFERENCES programs(programId)
+    FOREIGN KEY (profileId) REFERENCES profile(profileId),
+    FOREIGN KEY (programId) REFERENCES program(programId)
 );
 
 CREATE TABLE postTagParent (
@@ -111,25 +111,25 @@ CREATE TABLE postTagParent (
     category VARCHAR(100)
 );
 
-CREATE TABLE postTags (
+CREATE TABLE postTag (
     postId INT NOT NULL,
     postTagId INT NOT NULL,
-    FOREIGN KEY (postId) REFERENCES posts(postId) 
+    FOREIGN KEY (postId) REFERENCES post(postId)
 	    ON UPDATE cascade ON DELETE cascade,
     FOREIGN KEY (postTagId) REFERENCES postTagParent(postTagId) 
 	    ON UPDATE cascade ON DELETE cascade
 );
 
-INSERT INTO school (name, programs, professors) VALUES
-('Boston University', JSON_ARRAY('CIEE Monteverde - Sustainability and the Environment', ' Mexico City: Gender and Migration'), JSON_ARRAY('Dr. Smith', 'Dr. Ron')),
-('University of Southern California', JSON_ARRAY('Honors: Legal and philosophical perspectives on free speech and protest in France', 'Illegal Trade in Medical Products, Social Responsibility and Public Health'), JSON_ARRAY('Dr. Snow', 'Dr. Taylor'));
+INSERT INTO school (name, bio) VALUES
+('Boston University', 'Boston University is a private research university in Boston, Massachusetts, United States.'),
+('University of Southern California', 'The University of Southern California is a private research university in Los Angeles, California, United States.');
 
-INSERT INTO roles (name, canPost, canApprove, canAssignProf, canApply, canRetract, canEditOwn, canEditAll, canDeleteOwn, canDeleteAll, canUpdateAccess) VALUES
+INSERT INTO role (name, canPost, canApprove, canAssignProf, canApply, canRetract, canEditOwn, canEditAll, canDeleteOwn, canDeleteAll, canUpdateAccess) VALUES
 ('Admin', TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE),
 ('Professor', TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE),
 ('Student', FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE);
 
-INSERT INTO users (firstName, middleName, lastName, phone, email, schoolId, roleId) VALUES
+INSERT INTO user (firstName, middleName, lastName, phone, email, schoolId, roleId) VALUES
 ('Dao', 'Lain', 'Hope', '123-456-7890', 'dao@example.edu', 1, 3),
 ('Ben', NULL, 'June', '555-555-5555', 'ben@example.edu', 2, 3),
 ('Daniel', NULL, 'Smith', '098-765-4321', 'smith@example.edu', 1, 2),
@@ -137,27 +137,31 @@ INSERT INTO users (firstName, middleName, lastName, phone, email, schoolId, role
 ('White', 'Apple', 'Snow', '567-343-5678', 'snow@example.edu', 2, 2),
 ('Swift', NULL, 'Taylor', '345-222-3243', 'taylor@example.edu', 2, 2);
 
-INSERT INTO programs (title, location, schoolId, professorId, applicants, approvedApplicants, programStart, programEnd) VALUES
-('Dialogue: The Mathematical Heritage of Hungary', 'Hungary', 1, 1, JSON_ARRAY('Dao', 'Ben'), JSON_ARRAY('Alice'), '2024-01-01', '2024-06-01'),
-('Dialogue: The Chemistry of Green Energy in Iceland', 'Iceland', 2, 2, JSON_ARRAY(), JSON_ARRAY(), '2024-02-01', '2024-07-01');
-
 INSERT INTO userTagParent (tagName, category) VALUES
 ('Computer Science', 'Major'),
 ('2027', 'Graduation Year');
 
-INSERT INTO userTags (userId, userTagId) VALUES
+INSERT INTO userTag (userId, userTagId) VALUES
 (1, 1),
 (2, 2);
 
-INSERT INTO profiles (firstName, middleName, lastName, userId, bio, email, userTagsArray) VALUES
-('Dao', 'Lain', 'Hope', 1, 'Software Engineer looking for a dialogue!', 'dao@example.com', JSON_ARRAY('Computer Science', '2027')),
-('Ben', NULL, 'June', 2, 'Business Analyst specializing in market trends.', 'ben@example.com', JSON_ARRAY(NULL));
+INSERT INTO profile (firstName, middleName, lastName, userId, schoolId, bio, email) VALUES
+('Dao', 'Lain', 'Hope', 1, 1,'Software Engineer looking for a dialogue!', 'dao@example.com'),
+('Ben', NULL, 'June', 2, 2,'Business Analyst specializing in market trends.', 'ben@example.com'),
+('Daniel', NULL, 'Smith', 3, 1, 'Professor teaching Biology and is open to lead a dialogue', 'smith@example.edu'),
+('Weasley', 'Donald', 'Ron', 4, 1, 'Professor teaching Chemistry and is closed to leading a dialogue','ron@example.edu'),
+('White', 'Apple', 'Snow', 5, 2, 'An apple a day keeps the witch away', 'snow@example.edu'),
+('Swift', NULL, 'Taylor', 6, 2, 'Does arena tours as a side hustle', 'taylor@example.edu');
 
-INSERT INTO posts (postAuthor, title, body, profileId, programId, tags, favorited) VALUES
-('Dr. Smith', 'Dialogue: The Mathematical Heritage of Hungary', 'This is an introduction to computer science courses.', 1, 1, JSON_OBJECT(1, ), TRUE),
-('White Apple Snow', 'Dialogue: The Chemistry of Green Energy in Iceland', 'Explore the world of finance and business opportunities in Iceland.', 3, 2, JSON_OBJECT('content', '2027'), FALSE);
+INSERT INTO program (title, location, schoolId, professorId, programStart, programEnd) VALUES
+('Dialogue: The Mathematical Heritage of Hungary', 'Hungary', 1, 1, '2024-01-01', '2024-06-01'),
+('Dialogue: The Chemistry of Green Energy in Iceland', 'Iceland', 2, 2, '2024-02-01', '2024-07-01');
 
-INSERT INTO postTags (postId, postTagId) VALUES
+INSERT INTO post (postAuthor, title, body, profileId, programId, favorited) VALUES
+('Dr. Smith', 'Dialogue: The Mathematical Heritage of Hungary', 'This is an introduction to computer science courses.', 3, 1, TRUE),
+('White Apple Snow', 'Dialogue: The Chemistry of Green Energy in Iceland', 'Explore the world of finance and business opportunities in Iceland.', 5, 2, FALSE);
+
+INSERT INTO postTag (postId, postTagId) VALUES
 (1, 1),
 (2, 2);
 
