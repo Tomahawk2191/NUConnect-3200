f25e108bcf6de05a8cf601bc0140ba1de92f0ccd
tobychan44 <tobychch@gmail.com>
2024-11-19 19:59:44 -0500

    added application entity

--- Project.sql
+++ Project.sql
@@ -91,17 +91,15 @@ CREATE TABLE post (
     postAuthor VARCHAR(150) NOT NULL,
     title VARCHAR (100) NOT NULL,
     body VARCHAR (300),
-    profileId INT,
+    userId INT,
     programId INT,
     #thumbnail,
     published BOOLEAN DEFAULT false,
     favorited BOOLEAN DEFAULT false,
-    applied BOOLEAN DEFAULT false,
-    accepted BOOLEAN DEFAULT false,
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     lastEdited DATETIME DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (profileId) REFERENCES profile(profileId),
+    FOREIGN KEY (userId) REFERENCES user(userId),
     FOREIGN KEY (programId) REFERENCES program(programId)
 );
 
@@ -120,6 +118,17 @@ CREATE TABLE postTag (
 	    ON UPDATE cascade ON DELETE cascade
 );
 
+CREATE TABLE application (
+    applicationId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    programId INT,
+    applied BOOLEAN DEFAULT false,
+    accepted BOOLEAN DEFAULT false,
+    denied BOOLEAN DEFAULT false,
+    FOREIGN KEY (userId) REFERENCES user(userId),
+    FOREIGN KEY (programId) REFERENCES program(programId)
+);
+
 INSERT INTO school (name, bio) VALUES
 ('Boston University', 'Boston University is a private research university in Boston, Massachusetts, United States.'),
 ('University of Southern California', 'The University of Southern California is a private research university in Los Angeles, California, United States.');
@@ -130,12 +139,12 @@ INSERT INTO role (name, canPost, canApprove, canAssignProf, canApply, canRetract
 ('Student', FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE);
 
 INSERT INTO user (firstName, middleName, lastName, phone, email, schoolId, roleId) VALUES
-('Dao', 'Lain', 'Hope', '123-456-7890', 'dao@example.edu', 1, 3),
-('Ben', NULL, 'June', '555-555-5555', 'ben@example.edu', 2, 3),
 ('Daniel', NULL, 'Smith', '098-765-4321', 'smith@example.edu', 1, 2),
 ('Weasley', 'Donald', 'Ron', '111-222-3456', 'ron@example.edu', 1, 2),
 ('White', 'Apple', 'Snow', '567-343-5678', 'snow@example.edu', 2, 2),
-('Swift', NULL, 'Taylor', '345-222-3243', 'taylor@example.edu', 2, 2);
+('Swift', NULL, 'Taylor', '345-222-3243', 'taylor@example.edu', 2, 2),
+('Dao', 'Lain', 'Hope', '123-456-7890', 'dao@example.edu', 1, 3),
+('Ben', NULL, 'June', '555-555-5555', 'ben@example.edu', 2, 3);
 
 INSERT INTO userTagParent (tagName, category) VALUES
 ('Computer Science', 'Major'),
@@ -146,25 +155,29 @@ INSERT INTO userTag (userId, userTagId) VALUES
 (2, 2);
 
 INSERT INTO profile (firstName, middleName, lastName, userId, schoolId, bio, email) VALUES
-('Dao', 'Lain', 'Hope', 1, 1,'Software Engineer looking for a dialogue!', 'dao@example.com'),
-('Ben', NULL, 'June', 2, 2,'Business Analyst specializing in market trends.', 'ben@example.com'),
-('Daniel', NULL, 'Smith', 3, 1, 'Professor teaching Biology and is open to lead a dialogue', 'smith@example.edu'),
-('Weasley', 'Donald', 'Ron', 4, 1, 'Professor teaching Chemistry and is closed to leading a dialogue','ron@example.edu'),
-('White', 'Apple', 'Snow', 5, 2, 'An apple a day keeps the witch away', 'snow@example.edu'),
-('Swift', NULL, 'Taylor', 6, 2, 'Does arena tours as a side hustle', 'taylor@example.edu');
+('Daniel', NULL, 'Smith', 1, 1, 'Professor teaching Biology and is open to lead a dialogue', 'smith@example.edu'),
+('Weasley', 'Donald', 'Ron', 2, 1, 'Professor teaching Chemistry and is closed to leading a dialogue','ron@example.edu'),
+('White', 'Apple', 'Snow', 3, 2, 'An apple a day keeps the witch away', 'snow@example.edu'),
+('Swift', NULL, 'Taylor', 4, 2, 'Does arena tours as a side hustle', 'taylor@example.edu'),
+('Dao', 'Lain', 'Hope', 5, 1,'Software Engineer looking for a dialogue!', 'dao@example.com'),
+('Ben', NULL, 'June', 6, 2,'Business Analyst specializing in market trends.', 'ben@example.com');
 
 INSERT INTO program (title, location, schoolId, professorId, programStart, programEnd) VALUES
 ('Dialogue: The Mathematical Heritage of Hungary', 'Hungary', 1, 1, '2024-01-01', '2024-06-01'),
-('Dialogue: The Chemistry of Green Energy in Iceland', 'Iceland', 2, 2, '2024-02-01', '2024-07-01');
+('Dialogue: The Chemistry of Green Energy in Iceland', 'Iceland', 2, 3, '2024-02-01', '2024-07-01');
 
-INSERT INTO post (postAuthor, title, body, profileId, programId, favorited) VALUES
-('Dr. Smith', 'Dialogue: The Mathematical Heritage of Hungary', 'This is an introduction to computer science courses.', 3, 1, TRUE),
-('White Apple Snow', 'Dialogue: The Chemistry of Green Energy in Iceland', 'Explore the world of finance and business opportunities in Iceland.', 5, 2, FALSE);
+INSERT INTO post (postAuthor, title, body, programId, userId, favorited) VALUES
+('Dr. Smith', 'Dialogue: The Mathematical Heritage of Hungary', 'This is an introduction to computer science courses.', 1, 1, TRUE),
+('White Apple Snow', 'Dialogue: The Chemistry of Green Energy in Iceland', 'Explore the world of finance and business opportunities in Iceland.', 2, 3, FALSE);
+
+INSERT INTO postTagParent (tagName, category) VALUES
+('Summer 1', 'Time'),
+('Computer Science', 'Major');
 
 INSERT INTO postTag (postId, postTagId) VALUES
 (1, 1),
 (2, 2);
 
-INSERT INTO postTagParent (tagName, category) VALUES
-('Summer 1', 'Time'),
-('Computer Science', 'Major');
+INSERT INTO application (userId, programId, applied, accepted, denied) VALUES
+(1, 1, TRUE, FALSE, FALSE),
+(4, 2, TRUE, TRUE, FALSE);
