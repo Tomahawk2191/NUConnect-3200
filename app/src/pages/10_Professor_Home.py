8a7ce6abf2e315d66df9b0aee248b2478b6ecd25
pidang1 <dang.pi@northeastern.edu>
2024-12-02 20:07:40 -0500

    implemented professor's home page

--- app/src/pages/10_Professor_Home.py
+++ app/src/pages/10_Professor_Home.py
@@ -14,17 +14,17 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('Predict Value Based on Regression Model', 
+if st.button('My Programs', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/11_Prediction.py')
 
-if st.button('View the Simple API Demo', 
+if st.button('Browse Available Applications', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/12_API_Test.py')
 
-if st.button("View Classification Demo",
+if st.button("View Profile",
              type='primary',
              use_container_width=True):
   st.switch_page('pages/13_Classification.py')
