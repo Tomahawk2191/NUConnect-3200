0b7bf3687bb5a66795fde126a1ca96dbe542ff8d
zhou-cat <150718110+zhou-cat@users.noreply.github.com>
2024-12-04 02:15:23 -0500

    Created new files and rerouted pages
    Created new files for University Personas and rerouted buttons of persona to correct pages.

---
+++ app/src/pages/15_University_Home.py
@@ -0,0 +1,30 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome NUConnect, {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
+
+if st.button('Manage Avalible Programs', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/16_Uni_All_Programs.py')
+
+if st.button("Manage All Users",
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/17_Uni_All_Users.py')
+
+if st.button('View Profile', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/18_Uni_Profile.py')
